6a7c71e86f75d5dda016b7605fdf2899db8b8f3d
ayush sharma <ayush89able@gmail.com>
2019-08-28 23:39:55 +0530

    errors solved for deployed code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
+const API_URL = 'https://jsonplaceholder.typicode.com';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    const url = `${API_URL}/users/`;
+    axios.get(url).then(response => response.data)
+    .then((data) => {
+      this.setState({ users: data })
+      console.log(this.state.users)
+     })
+    }
+     render() {
+      return (
+        <div className="container">
+            <div className="col-xs-8">
+            <h1 style={{textAlign:'center'}}>Users Registered with us</h1>
+            {this.state.users.map((user) => (
+              <div  key={user.id} className="card">
+              <div className="card-body">
+                  <h5 className="card-title">{user.name}</h5>
+                  <h6>Email: {user.email}</h6>     
+                  <h6>Contact: {user.phone}</h6>
+                  <h6>Website: {user.website}</h6>
+                  <h6>Company: {user.company.name}</h6>    
+                  <h6>Address: {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}</h6> 
+                </div>
+              </div> 
+            ))}
+            </div>
+          </div>
+      );
+  }
 }
-
 export default App;
