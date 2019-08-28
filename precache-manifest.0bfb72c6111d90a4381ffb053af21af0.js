d131538701b82dfc23646ad06f175c12b758334a
ayush sharma <ayush89able@gmail.com>
2019-08-28 22:43:56 +0530

    Updates

---
+++ precache-manifest.0bfb72c6111d90a4381ffb053af21af0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "039e4c4c1a6fe3816560",
+    "url": "/myReactAppTwo/static/js/runtime~main.abfcb397.js"
+  },
+  {
+    "revision": "b3d66400fab99a59b329",
+    "url": "/myReactAppTwo/static/js/main.36fb8cc5.chunk.js"
+  },
+  {
+    "revision": "d037dc94d041ad147999",
+    "url": "/myReactAppTwo/static/js/2.65045a5b.chunk.js"
+  },
+  {
+    "revision": "b3d66400fab99a59b329",
+    "url": "/myReactAppTwo/static/css/main.4ff0acd7.chunk.css"
+  },
+  {
+    "revision": "130127d4bbee425107c7283a8fd82e44",
+    "url": "/myReactAppTwo/index.html"
+  }
+];
